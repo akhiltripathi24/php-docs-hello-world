bf49375864a78811e89dc6466106e088e419accb
Akhil Tripathi <akhil.uiet83@gmail.com>
2021-09-04 12:51:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
   echo ("I am deploying it to Azure and github");
   echo ("This is super easy");
   echo ("Hi I am doing this again for testing.");
+  echo ("I am redelpoying it on Azure and Updating by github") 
 ?>
